88f9f8413af9bb568f97d69c2bb75dc8d861258f
Hiren Khunt <hirenkhunt5960@gmail.com>
2023-06-03 05:28:07 +0000

    Product upload changes

--- frontend/gardenbuzz/src/Controllers/uploadFiles/addImage.js
+++ frontend/gardenbuzz/src/Controllers/uploadFiles/addImage.js
@@ -9,7 +9,7 @@ function AddImage() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
   const [description, setDescription] = useState("");
-  const [potColor, setPotColor] = useState("white");
+  const [category, setCategory] = useState("plant");
   const [careInstructions, setCareInstructions] = useState("");
   const [productQuantity, setProductQuantity] = useState(0);
 
@@ -21,21 +21,24 @@ function AddImage() {
     await uploadBytes(imageRef, imageUpload);
 
     const url = await getDownloadURL(imageRef);
-    const response = await fetch("http://localhost:4200/gardenbuzz/add_product", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        Name: name,
-        Price: price,
-        Description: description,
-        PotColor: potColor,
-        CareInstructions: careInstructions,
-        ProductQuantity: productQuantity,
-        ImageURL: url,
-      }),
-    });
+    const response = await fetch(
+      "http://localhost:4200/gardenbuzz/add_product",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          Name: name,
+          Price: price,
+          Description: description,
+          Category: category,
+          CareInstructions: careInstructions,
+          ProductQuantity: productQuantity,
+          ImageURL: url,
+        }),
+      }
+    );
     const data = await response.json();
     alert("Product Uploaded Successfully!");
     window.location.href = "/Upload";
@@ -44,85 +47,80 @@ function AddImage() {
   return (
     <>
       <form onSubmit={uploadFile} className="form">
-  <div className="form-column">
-    <label htmlFor="product-name">Name:</label>
-    <input
-      type="text"
-      id="product-name"
-      value={name}
-      onChange={(e) => setName(e.target.value)}
-      placeholder="Product Name"
-      required
-    />
+        <div className="form-column">
+          <label htmlFor="product-name">Name:</label>
+          <input
+            type="text"
+            id="product-name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Product Name"
+            required
+          />
 
-    <label htmlFor="price">Price:</label>
-    <input
-      type="number"
-      id="price"
-      name="price"
-      min="0"
-      step="1"
-      value={price}
-      onChange={(e) => setPrice(parseFloat(e.target.value))}
-      required
-    />
+          <label htmlFor="price">Price:</label>
+          <input
+            type="number"
+            id="price"
+            name="price"
+            min="0"
+            step="1"
+            value={price}
+            onChange={(e) => setPrice(parseFloat(e.target.value))}
+            required
+          />
 
-    <label htmlFor="description">Description:</label>
-    <textarea
-      id="description"
-      name="description"
-      rows="1"
-      value={description}
-      onChange={(e) => setDescription(e.target.value)}
-    ></textarea>
-  </div>
+          <label htmlFor="description">Description:</label>
+          <textarea
+            id="description"
+            name="description"
+            rows="1"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
 
-  <div className="form-column">
-    <label htmlFor="pots-color">Pot Color:</label>
-    <select
-      id="pots-color"
-      name="pots-color"
-      value={potColor}
-      onChange={(e) => setPotColor(e.target.value)}
-    >
-      <option value="white">White</option>
-      <option value="black">Black</option>
-      <option value="red">Red</option>
-      <option value="blue">Blue</option>
-      <option value="green">Green</option>
-    </select>
+          <label htmlFor="pots-color">Category:</label>
+          <select
+            id="pots-color"
+            name="pots-color"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="plant">Plant</option>
+            <option value="flower">Flowers</option>
+            <option value="vine">Vine</option>
+            <option value="plantcare"> Plant Care Product</option>
+            <option value="fertilizer">Fertilizer</option>
+          </select>
 
-    <label htmlFor="care-instructions">Care Instructions:</label>
-    <textarea
-      id="care-instructions"
-      name="care-instructions"
-      rows="1"
-      value={careInstructions}
-      onChange={(e) => setCareInstructions(e.target.value)}
-    ></textarea>
+          <label htmlFor="care-instructions">Instructions:</label>
+          <textarea
+            id="care-instructions"
+            name="care-instructions"
+            rows="1"
+            value={careInstructions}
+            onChange={(e) => setCareInstructions(e.target.value)}
+          ></textarea>
 
-    <label htmlFor="product-quantity">Product Quantity:</label>
-    <input
-      type="number"
-      id="product-quantity"
-      name="product-quantity"
-      min="0"
-      value={productQuantity}
-      onChange={(e) => setProductQuantity(e.target.value)}
-      required
-    ></input>
-  </div>
+          <label htmlFor="product-quantity">Product Quantity:</label>
+          <input
+            type="number"
+            id="product-quantity"
+            name="product-quantity"
+            min="0"
+            value={productQuantity}
+            onChange={(e) => setProductQuantity(e.target.value)}
+            required
+          ></input>
 
-  <div className="form-column">
-    <input
-      type="file"
-      onChange={(event) => setImageUpload(event.target.files[0])}
-      required
-    />
-    <input type="submit" name="submit" value="Submit" />
-  </div>
-</form>
-
+          <input
+            type="file"
+            onChange={(event) => setImageUpload(event.target.files[0])}
+            required
+          />
+          <input type="submit" name="submit" value="Submit" />
+        </div>
+      </form>
     </>
   );
 }
